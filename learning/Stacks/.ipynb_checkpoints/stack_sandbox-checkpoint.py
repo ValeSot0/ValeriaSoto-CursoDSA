282af1b344b14fd7ef401451e9e7db766042a860
Valeria Soto Hernandez <230110118@itsoeh.edu.mx>
2024-12-10 22:41:06 -0600

    Stacks Test 3 & HanoiGame
    Signed-off-by: Valeria Soto Hernandez <230110118@itsoeh.edu.mx>

--- learning/Stacks/.ipynb_checkpoints/stack_sandbox-checkpoint.py
+++ learning/Stacks/.ipynb_checkpoints/stack_sandbox-checkpoint.py
@@ -1,19 +1,38 @@
 from Node import Node
+import logging
+
 class Stack:
-    def __init__(self, top_item = None):
+    def __init__(self, top_item=None, limit=1000, size=0):
         self.top_item = top_item
+        self.limit = limit
+        self.size = size
+
+    def has_space(self):
+        return self.limit > self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def peek(self):
+        if (not self.is_empty()):
+            return self.top_item.get_value()
+        else:
+            logging.warning("La pila esta totalmente vacia!")
 
     def push(self, value):
-        new_node = Node(value)
-        new_node.set_next_node(self.top_item)
-        self.top_item = new_node
+        if (self.has_space()):
+            item = Node(value)
+            item.set_next_node(self.top_item)
+            self.top_item = item
+            self.size += 1
+        else:
+            logging.warning("La pila esta llena ¡No queda espacio!")
 
     def pop(self):
-        if self.top_item is None:
-            return None
         item_to_remove = self.top_item
-        self.top_item = self.top_item.get_next_node()
-        return item_to_remove.get_value()
-    
-    def peek(self):
-        return self.top_item.get_value()
+        if (not self.is_empty()):
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            logging.warning("La pila esta totalmente vacia!")
