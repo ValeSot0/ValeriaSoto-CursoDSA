39b823a969e6b88108554aa0d495ef9e7f5b200d
Valeria Soto Hernandez <230110118@itsoeh.edu.mx>
2024-12-10 22:30:31 -0600

    Stacks Test 2
    Signed-off-by: Valeria Soto Hernandez <230110118@itsoeh.edu.mx>

--- learning/Stacks/stack_sandbox.py
+++ learning/Stacks/stack_sandbox.py
@@ -1,9 +1,19 @@
 from Node import Node
-import logging
-
-class Stack():
+class Stack:
     def __init__(self, top_item = None):
         self.top_item = top_item
 
+    def push(self, value):
+        new_node = Node(value)
+        new_node.set_next_node(self.top_item)
+        self.top_item = new_node
+
+    def pop(self):
+        if self.top_item is None:
+            return None
+        item_to_remove = self.top_item
+        self.top_item = self.top_item.get_next_node()
+        return item_to_remove.get_value()
+    
     def peek(self):
         return self.top_item.get_value()
